ae062d3e49070cd35690131c15942b868b396e60
Lydien Sandanasamy <s.lydien@icloud.com>
2021-01-05 18:24:21 +0100

    Feature: Adding rule for lxappearance and megasync

--- config.def.h
+++ config.def.h
@@ -30,7 +30,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "lxappearance",  NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "lxappearance",  "Lxappearance",       "Customize Look and Feel",       0,       1,           -1 },
+	{ "megasync",  "MEGAsync",       NULL,       0,       1,           -1 },
 };
 
 /* layout(s) */
