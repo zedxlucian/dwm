7a5cab5eadaa56cb3e1d37404360fa59cb0b5765
Lydien Sandanasamy <s.lydien@icloud.com>
2021-01-14 13:25:18 +0100

    Revert "Revert "fixup! Feature: Add fuzzymatch and case-insensitive dmenu""
    This reverts commit 581a33d672c95415852939603e2c89d81b147108.

--- config.def.h
+++ config.def.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-F", "-i", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *emacscmd[]  = { "emacsclient", "-c", NULL };
 static const char *togglethemecmd[]  = { "change_mode", "toggle", NULL };
